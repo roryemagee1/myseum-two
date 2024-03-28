7c23d4d09c572e540460a7f49dff8dcb9a270e28
Rory Magee <roryemagee@gmail.com>
2024-03-28 13:16:40 -0600

    Add level rotator to gravity_box

--- Levels/Environment.gd
+++ Levels/Environment.gd
@@ -1,5 +1,17 @@
 extends Node3D
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("one"):
+	if event.is_action_pressed("rot_x_plus"):
 		rotation_degrees.x += 90
+	if event.is_action_pressed("rot_x_minus"):
+		rotation_degrees.x -= 90
+	if event.is_action_pressed("rot_y_plus"):
+		rotation_degrees.y += 90
+	if event.is_action_pressed("rot_y_minus"):
+		rotation_degrees.y -= 90
+		
+	#if event.is_action_pressed("rot_z_plus"):
+		#rotation_degrees.z += 90
+	#if event.is_action_pressed("rot_z_minus"):
+		#rotation_degrees.z -= 90
+		
