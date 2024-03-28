082318cca814e10821c18d2369420c54bcf3e477
Rory Magee <roryemagee@gmail.com>
2024-03-28 16:42:06 -0600

    Bookmark gravity_box attempt 2 so that I can comeback to it if attempt 3 does not work either

--- Player/gravity_player.gd
+++ Player/gravity_player.gd
@@ -6,9 +6,7 @@ const SPEED = 5.0
 @export var fall_multiplier: float = 2.0
 
 #Temp?
-@export var gravity_y: bool = true
-@export var gravity_x: bool = false
-@export var gravity_z: bool = false
+@export var gravity_direction: String = "y"
 #Temp?
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -22,27 +20,42 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
+	handle_gravity(delta, gravity_direction)
+
+func handle_gravity(delta: float, gravity_direction: String) -> void:
 	# Add the gravity.
+	var vector := ["x", "y", "z", 1]
+	if gravity_direction == "-y":
+		vector = ["x", "y", "z", -1]
+	if gravity_direction == "x":
+		vector = ["z", "x", "y", 1]
+	if gravity_direction == "-x":
+		vector = ["z", "x", "y", -1]
+	if gravity_direction == "z":
+		vector = ["y", "z", "x", 1]
+	if gravity_direction == "-z":
+		vector = ["y", "z", "x", -1]
+	
 	if not is_on_floor():
-		if velocity.y >= 0:
-			velocity.y -= gravity * delta
+		if velocity[vector[1]] >= 0:
+			velocity[vector[1]] -= gravity * vector[3] * delta
 		else:
-			velocity.y -= gravity * delta * fall_multiplier
+			velocity[vector[1]] -= gravity * vector[3] * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = sqrt(jump_height * 2.0 * gravity)
+		velocity[vector[1]] = sqrt(jump_height * 2.0 * gravity * vector[3])
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir[vector[0]], 0, input_dir[vector[1]])).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity[vector[0]] = direction[vector[0]] * SPEED
+		velocity[vector[2]] = direction[vector[2]] * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity[vector[0]] = move_toward(velocity[vector[0]], 0, SPEED)
+		velocity[vector[2]] = move_toward(velocity[vector[2]], 0, SPEED)
 
 	move_and_slide()
 	
@@ -56,7 +69,17 @@ func _input(event: InputEvent) -> void:
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if event.is_action_pressed("one"):
-		return_to_origin()
+		gravity_direction = "y"
+	if event.is_action_pressed("two"):
+		gravity_direction = "-y"
+	if event.is_action_pressed("three"):
+		gravity_direction = "x"
+	if event.is_action_pressed("four"):
+		gravity_direction = "-x"
+	if event.is_action_pressed("five"):
+		gravity_direction = "z"
+	if event.is_action_pressed("six"):
+		gravity_direction = "-z"
 	
 		
 func handle_camera_rotation() -> void:
@@ -71,3 +94,28 @@ func handle_camera_rotation() -> void:
 
 func return_to_origin() -> void:
 	position = Vector3(0.0, 1.0, 0.0)
+	
+#Save
+#func handle_gravity() -> void:
+	#if not is_on_floor():
+		#if velocity.y >= 0:
+			#velocity.y -= gravity * delta
+		#else:
+			#velocity.y -= gravity * delta * fall_multiplier
+#
+	# Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = sqrt(jump_height * 2.0 * gravity)
+#
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+#
+	#move_and_slide()
