6b70ce00884825a0b9c38747bd497ea89f4f4a28
Rory Magee <roryemagee@gmail.com>
2024-04-10 14:59:15 -0600

    Add About camera

--- UserInterface/basic_ui.gd
+++ UserInterface/basic_ui.gd
@@ -1,5 +1,14 @@
 extends Control
 
+@export var default_background: ViewportTexture
+@onready var menu_background: TextureRect = $MenuBackground
+@onready var myseum_one_viewport: SubViewport = %MyseumOneViewport
+@onready var player: CharacterBody3D = %Player
+@onready var myseum_one_camera: Camera3D = %MyseumOneCamera
+
+func _ready() -> void:
+	menu_background.set_background(myseum_one_viewport.get_texture())
+
 func toggle_ui(ui_visible: bool) -> void:
 	visible = ui_visible
 	get_tree().paused = ui_visible
@@ -22,3 +31,5 @@ func _on_gallery_button_pressed() -> void:
 
 func _on_enter_button_pressed() -> void:
 	toggle_ui(false)
+	player.position = myseum_one_camera.position
+	
