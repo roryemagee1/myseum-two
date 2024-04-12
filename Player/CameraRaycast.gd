5cc795632a91eceab94331342de25a71d792dd2e
Rory Magee <roryemagee@gmail.com>
2024-04-11 18:05:00 -0600

    Add functioning rotation buttons that change color when you hover your cursor over them

--- Player/CameraRaycast.gd
+++ Player/CameraRaycast.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
 	#if get_collider() != null:
 		#print(get_collider().is_in_group("rot_x_minus"))
-	print(is_colliding())
+	#print(is_colliding())
 	
